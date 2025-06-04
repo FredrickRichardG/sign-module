e7d6b3fbd550adac75979654b88f2b59c89f8b36
FredrickRichardG <fredrick.gnanaraj@ideas2it.com>
2025-06-04 20:05:46 +0530

    vital sign keyloak changes

--- target/generated-sources/annotations/com/healthcare/vitalsigns/mapper/VitalSignsMapperImpl.java
+++ target/generated-sources/annotations/com/healthcare/vitalsigns/mapper/VitalSignsMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-03T15:32:47+0530",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
+    date = "2025-06-03T17:01:14+0530",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
 public class VitalSignsMapperImpl implements VitalSignsMapper {
@@ -21,19 +21,19 @@ public class VitalSignsMapperImpl implements VitalSignsMapper {
 
         VitalSignsDTO vitalSignsDTO = new VitalSignsDTO();
 
-        vitalSignsDTO.setId( entity.getId() );
-        vitalSignsDTO.setPatientId( entity.getPatientId() );
-        vitalSignsDTO.setPulse( entity.getPulse() );
-        vitalSignsDTO.setSystolicBP( entity.getSystolicBP() );
-        vitalSignsDTO.setDiastolicBP( entity.getDiastolicBP() );
-        vitalSignsDTO.setTemperature( entity.getTemperature() );
-        vitalSignsDTO.setRespirations( entity.getRespirations() );
         vitalSignsDTO.setBloodSugar( entity.getBloodSugar() );
-        vitalSignsDTO.setWeight( entity.getWeight() );
+        vitalSignsDTO.setDiastolicBP( entity.getDiastolicBP() );
         vitalSignsDTO.setHeight( entity.getHeight() );
-        vitalSignsDTO.setSpo2Saturation( entity.getSpo2Saturation() );
-        vitalSignsDTO.setPtInr( entity.getPtInr() );
+        vitalSignsDTO.setId( entity.getId() );
         vitalSignsDTO.setNotes( entity.getNotes() );
+        vitalSignsDTO.setPatientId( entity.getPatientId() );
+        vitalSignsDTO.setPtInr( entity.getPtInr() );
+        vitalSignsDTO.setPulse( entity.getPulse() );
+        vitalSignsDTO.setRespirations( entity.getRespirations() );
+        vitalSignsDTO.setSpo2Saturation( entity.getSpo2Saturation() );
+        vitalSignsDTO.setSystolicBP( entity.getSystolicBP() );
+        vitalSignsDTO.setTemperature( entity.getTemperature() );
+        vitalSignsDTO.setWeight( entity.getWeight() );
 
         return vitalSignsDTO;
     }
@@ -46,19 +46,19 @@ public class VitalSignsMapperImpl implements VitalSignsMapper {
 
         VitalSigns vitalSigns = new VitalSigns();
 
-        vitalSigns.setId( dto.getId() );
-        vitalSigns.setPatientId( dto.getPatientId() );
-        vitalSigns.setPulse( dto.getPulse() );
-        vitalSigns.setSystolicBP( dto.getSystolicBP() );
-        vitalSigns.setDiastolicBP( dto.getDiastolicBP() );
-        vitalSigns.setTemperature( dto.getTemperature() );
-        vitalSigns.setRespirations( dto.getRespirations() );
         vitalSigns.setBloodSugar( dto.getBloodSugar() );
-        vitalSigns.setWeight( dto.getWeight() );
+        vitalSigns.setDiastolicBP( dto.getDiastolicBP() );
         vitalSigns.setHeight( dto.getHeight() );
-        vitalSigns.setSpo2Saturation( dto.getSpo2Saturation() );
-        vitalSigns.setPtInr( dto.getPtInr() );
+        vitalSigns.setId( dto.getId() );
         vitalSigns.setNotes( dto.getNotes() );
+        vitalSigns.setPatientId( dto.getPatientId() );
+        vitalSigns.setPtInr( dto.getPtInr() );
+        vitalSigns.setPulse( dto.getPulse() );
+        vitalSigns.setRespirations( dto.getRespirations() );
+        vitalSigns.setSpo2Saturation( dto.getSpo2Saturation() );
+        vitalSigns.setSystolicBP( dto.getSystolicBP() );
+        vitalSigns.setTemperature( dto.getTemperature() );
+        vitalSigns.setWeight( dto.getWeight() );
 
         return vitalSigns;
     }
@@ -69,44 +69,44 @@ public class VitalSignsMapperImpl implements VitalSignsMapper {
             return;
         }
 
-        if ( dto.getId() != null ) {
-            entity.setId( dto.getId() );
-        }
-        if ( dto.getPatientId() != null ) {
-            entity.setPatientId( dto.getPatientId() );
-        }
-        if ( dto.getPulse() != null ) {
-            entity.setPulse( dto.getPulse() );
-        }
-        if ( dto.getSystolicBP() != null ) {
-            entity.setSystolicBP( dto.getSystolicBP() );
+        if ( dto.getBloodSugar() != null ) {
+            entity.setBloodSugar( dto.getBloodSugar() );
         }
         if ( dto.getDiastolicBP() != null ) {
             entity.setDiastolicBP( dto.getDiastolicBP() );
         }
-        if ( dto.getTemperature() != null ) {
-            entity.setTemperature( dto.getTemperature() );
-        }
-        if ( dto.getRespirations() != null ) {
-            entity.setRespirations( dto.getRespirations() );
-        }
-        if ( dto.getBloodSugar() != null ) {
-            entity.setBloodSugar( dto.getBloodSugar() );
-        }
-        if ( dto.getWeight() != null ) {
-            entity.setWeight( dto.getWeight() );
-        }
         if ( dto.getHeight() != null ) {
             entity.setHeight( dto.getHeight() );
         }
-        if ( dto.getSpo2Saturation() != null ) {
-            entity.setSpo2Saturation( dto.getSpo2Saturation() );
+        if ( dto.getId() != null ) {
+            entity.setId( dto.getId() );
+        }
+        if ( dto.getNotes() != null ) {
+            entity.setNotes( dto.getNotes() );
+        }
+        if ( dto.getPatientId() != null ) {
+            entity.setPatientId( dto.getPatientId() );
         }
         if ( dto.getPtInr() != null ) {
             entity.setPtInr( dto.getPtInr() );
         }
-        if ( dto.getNotes() != null ) {
-            entity.setNotes( dto.getNotes() );
+        if ( dto.getPulse() != null ) {
+            entity.setPulse( dto.getPulse() );
+        }
+        if ( dto.getRespirations() != null ) {
+            entity.setRespirations( dto.getRespirations() );
+        }
+        if ( dto.getSpo2Saturation() != null ) {
+            entity.setSpo2Saturation( dto.getSpo2Saturation() );
+        }
+        if ( dto.getSystolicBP() != null ) {
+            entity.setSystolicBP( dto.getSystolicBP() );
+        }
+        if ( dto.getTemperature() != null ) {
+            entity.setTemperature( dto.getTemperature() );
+        }
+        if ( dto.getWeight() != null ) {
+            entity.setWeight( dto.getWeight() );
         }
     }
 }
