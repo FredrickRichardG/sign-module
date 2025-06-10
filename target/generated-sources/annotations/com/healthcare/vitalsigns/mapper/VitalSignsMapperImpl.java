2bfd325d751266f64aaf5c7d402ceceaf6abf202
FredrickRichardG <fredrick.gnanaraj@ideas2it.com>
2025-06-10 22:31:55 +0530

    adding mongodb

--- target/generated-sources/annotations/com/healthcare/vitalsigns/mapper/VitalSignsMapperImpl.java
+++ target/generated-sources/annotations/com/healthcare/vitalsigns/mapper/VitalSignsMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-10T15:39:30+0530",
+    date = "2025-06-10T21:45:53+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
